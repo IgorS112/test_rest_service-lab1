b4d923223019d5d10911773e5ec67fa0e93058ac
IgorS112 <127881064+IgorS112@users.noreply.github.com>
2023-03-14 22:36:00 +0500

    Initial commit

---
+++ src/main/java/ru/students/test_rest_service/model/Request.java
@@ -0,0 +1,29 @@
+package ru.students.test_rest_service.model;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+public class Request {
+
+        @NotBlank
+        @Size(max = 32)
+        private String uid;
+        private String    operationUid;
+        private String    systemName;
+        @NotBlank
+        private String    systemTime;
+        private String    source;
+        private int    communicationId;
+        private int     templateId;
+        private int     productCode;
+        private int     smsCode;
+    }
+
+
+
